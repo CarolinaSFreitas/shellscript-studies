35cc2a54d6b858bbc8fe072589d61a2c0562b9cd
Carolina Freitas <carolsoaresfreitas@hotmail.com>
2024-01-12 15:54:43 -0300

    processos memoria e consertos

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-converte_imagem(){
+converte_imagem() {
     local caminho_imagem=$1
     local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
     converte $imagem_sem_extensao.jpg $imagem_sem_extensao.png
 }
 
-varrer_diretorio(){
+varrer_diretorio() {
     cd $1
 
-    for arquivo in *
+    for arquivo in * 
     do
         local caminho_arquivo=$(find ~/Downloads/imagens-novos-livros -name $arquivo)
-        if [ -d $caminho_arquivo ]
-        then 
+        if [ -d $caminho_arquivo ] then
             varrer_diretorio $caminho_arquivo
         else
-           converte_imagem $caminho_arquivo
-        if
+            converte_imagem $caminho_arquivo
+        fi
     done
 }
 
 varrer_diretorio ~/Downloads/imagens-novos-livros
-if [ $? = 0 ]
-then 
+if [ $? = 0 ] then
     echo "Conversão feita com sucesso"
-else 
+else
     echo "Houve uma falha na conversão"
 fi
