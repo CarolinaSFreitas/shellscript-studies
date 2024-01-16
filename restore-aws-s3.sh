8f586a6c9159ce5bc3a8dee91abaabfab01a56a3
Carolina Freitas <carolsoaresfreitas@hotmail.com>
2024-01-16 10:59:13 -0300

    fix restore s3

--- restore-aws-s3.sh
+++ restore-aws-s3.sh
@@ -5,9 +5,11 @@ CAMINHO_RESTORE=/home/carol/restore-mutillidae-aws
 aws s3 sync s3://curso-shell-script/$(date +%F) $CAMINHO_RESTORE
 
 cd $CAMINHO_RESTORE
-if [ -f $1.sql ] then #'-f' = file
+if [ -f $1.sql ] 
+then #'-f' = file
     mysql -u root mutillidae <$1.sql
-    if [$? -eq 0] then
+    if [$? -eq 0] 
+    then
         echo "O restore foi feito com sucesso! :)"
     fi
 else
